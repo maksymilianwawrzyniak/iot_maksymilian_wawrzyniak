0ce44f75eb4703f620dde5ef42f22be8c3924bdc
Maksymilian <maksymilianwawrzyniak@gmail.com>
2020-12-14 01:38:06 +0100

    lab7 - chat signalR web app

---
+++ lab7/ChatSignalR/ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatSignalR.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(ChatMessage message)
+        {
+            Clients.All.SendAsync("RecievedMessage", message);
+        }
+
+        public void SignInUser(string userName)
+        {
+            Clients.All.SendAsync("UserSignedIn", userName);
+        }
+    }
+}
---
+++ lab7/ChatSignalR/ChatSignalR/wwwroot/appScripts.js
@@ -0,0 +1,53 @@
+﻿(function() {
+    var btnSend = $('#btnSend');
+    var tBoxMsg = $('#tBoxMsg');
+    var chatList = $('#chatList');
+    var userName = $('#user-name').val();
+
+    var connection = new signalR.HubConnectionBuilder()
+        .withUrl("/chat")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    $(btnSend).click(function() {
+        var msg = $(tBoxMsg).val();
+
+        connection.invoke('SendMessage',
+            {
+                userName: userName,
+                message: msg
+            });
+    });
+
+    connection.on('RecievedMessage',
+        function (obj) {
+            var htmlElement = '<li>'
+                + '<span class="font-weight-bold">['
+                + obj.formattedTimeStamp + '] '
+                + obj.userName
+                + '</span>: '
+                + obj.message
+                + '</li>';
+
+            $(tBoxMsg).val('');
+
+            $(chatList).prepend(htmlElement);
+        });
+
+    connection.on('UserSignedIn',
+        function (obj) {
+            var htmlElement = '<li>'
+                + '<span class="font-weight-bold text-success">'
+                + obj + ' joined chat!'
+                + '</li>';
+
+            $(tBoxMsg).val('');
+
+            $(chatList).prepend(htmlElement);
+        });
+
+    connection.start().then(function() {
+        console.log("connected as " + userName);
+        connection.invoke('SignInUser', userName);
+    });
+})();
